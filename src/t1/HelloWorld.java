d0ed494dcc872cd86dc736b4974be5aaf0b474aa
vanth2002 <huanhmho0@gmail.com>
2024-10-22 18:20:16 +0700

    init project

---
+++ src/t1/HelloWorld.java
@@ -0,0 +1,12 @@
+package t1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world with java and huan");
+		System.out.println("hello world with java and huan");
+
+		System.out.println("hello world with java and huan");
+		System.out.println("hello world with java and huan");
+
+	}
+}
